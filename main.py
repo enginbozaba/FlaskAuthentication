feeac1cb28446b72fb76a531d9556c5c8a130dc4
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-12 10:37:03 +0000

    Add flask-login configs

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import os
 from flask import Flask, render_template, request, redirect, url_for, send_from_directory
 from flask_sqlalchemy import SQLAlchemy
-from flask_login import UserMixin
-from werkzeug.security import generate_password_hash
+from flask_login import UserMixin, LoginManager, login_required, login_user, current_user
+from werkzeug.security import generate_password_hash, check_password_hash
 from dotenv import load_dotenv
 load_dotenv()
 
@@ -13,6 +13,14 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    return Users.query.get(user_id)
+
 
 # Users Table
 class Users(UserMixin, db.Model):
@@ -22,16 +30,26 @@ class Users(UserMixin, db.Model):
     password = db.Column(db.String(500), nullable=False)
 
 
-db.create_all()
-
+# db.create_all()
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+
+        # Find the user by email
+        user = Users.query.filter_by(email=email).first()
+
+        # Check stored password hash against entered password hashed.
+        if check_password_hash(user.password, password):
+            login_user(user)
+            return redirect(url_for('dashboard'))
     return render_template('login.html')
 
 
@@ -51,16 +69,22 @@ def register():
         )
         db.session.add(new_user)
         db.session.commit()
+
+        # Log in and authenticate user after adding details to database.
+        login_user(new_user)
+
         return redirect(url_for('dashboard'))
     return render_template('register.html')
 
 
 @app.route('/dashboard')
+@login_required
 def dashboard():
-    return render_template('dashboard.html')
+    return render_template('dashboard.html', name=current_user.name)
 
 
 @app.route('/download')
+@login_required
 def download():
     return send_from_directory('static', filename="cheat_sheet.pdf")
 
