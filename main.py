701db2bef213768d90b8dda0d149e856d4218af5
A3AJAGBE <ajagbeaminat95@gmail.com>
2021-03-12 09:19:51 +0000

    Add users table

--- main.py
+++ main.py
@@ -1,6 +1,27 @@
+import os
 from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_login import UserMixin
+from dotenv import load_dotenv
+load_dotenv()
+
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = os.environ.get('SECRET_KEY')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///users.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+
+# Users Table
+class Users(UserMixin, db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(1000), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(500), nullable=False)
+
+
+db.create_all()
 
 
 @app.route('/')
